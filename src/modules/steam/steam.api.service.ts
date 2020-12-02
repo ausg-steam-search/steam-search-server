2515b5a667066616a9e5353f8f8ef45f3bd00533
Beygee <doug0476@naver.com>
2020-12-02 19:19:51 +0900

    리뷰 Cron

--- src/modules/steam/steam.api.service.ts
+++ src/modules/steam/steam.api.service.ts
@@ -10,8 +10,40 @@ import { delay } from 'src/utils/utils'
 export class SteamApiService {
   constructor(private readonly steamGameRepository: SteamGameRepository) {}
 
-  // @Cron('*/3 * * * * *')
-  public async scheduleGame() {
+  /**
+   * 주기적으로 게임 리뷰를 받아와 DB 적재
+   */
+  @Cron('0 * * * * *')
+  private async scheduleFetchReviews_0() {
+    await this.fetchReviews({ skip: 0, take: 200 })
+  }
+  @Cron('15 * * * * *')
+  private async scheduleFetchReviews_1() {
+    await this.fetchReviews({ skip: 200, take: 200 })
+  }
+  @Cron('30 * * * * *')
+  private async scheduleFetchReviews_2() {
+    await this.fetchReviews({ skip: 400, take: 200 })
+  }
+  @Cron('45 * * * * *')
+  private async scheduleFetchReviews_3() {
+    await this.fetchReviews({ skip: 600, take: 200 })
+  }
+
+  private async fetchReviews({ skip, take }: { skip: number; take: number }) {
+    const { items: games } = await this.steamGameRepository.findAll({ skip, take })
+
+    // await Promise.all(
+    //   games.map(async (game) => {
+    //     // const { data } = await steamReviewAxios.get(`/appreviews/945360?json=1&language=korean`)
+    //     // const reviews = data.reviews as ISteamReview[]
+    //     // console.log(reviews.length)
+    //     // console.log(reviews[0].author)
+    //   }),
+    // )
+  }
+
+  private async scheduleGame() {
     for (let appId = 945360; appId < 945361; appId += 10) {
       try {
         const { data } = await steamReviewAxios.get(
@@ -47,14 +79,5 @@ export class SteamApiService {
         await delay(200)
       }
     }
-  }
-
-  // https://store.steampowered.com
-  // @Cron('*/3 * * * * *')
-  private async scheduleReview() {
-    const { data } = await steamReviewAxios.get(`/appreviews/945360?json=1&language=korean`)
-
-    const reviews = data.reviews as ISteamReview[]
-    console.log(reviews[0].author)
   }
 }
